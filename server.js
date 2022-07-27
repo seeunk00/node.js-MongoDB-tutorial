48e5c6dee68424fe7e486627b7c5c969ab07342b
Seeun Kim <ubbke66@naver.com>
2022-07-28 08:43:11 +0900

    practice

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.get('/pet', function(req, res){
 app.get('/beauty', function(req, res){
 	res.send('뷰티 용품 쇼핑할 수 있는 페이지입니다.');
 });
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
