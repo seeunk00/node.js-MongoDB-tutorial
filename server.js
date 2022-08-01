e853a4bea65f76572f0a0bfc3f7676a81f5089a3
Seeun Kim <ubbke66@naver.com>
2022-08-02 08:48:34 +0900

    mongoDB error

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
+const { MongoClient } = require('mongodb');
 
+var db;
+const uri = "mongodb+srv://admin:admin1234@cluster0.irzrs.mongodb.net/todoapp?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
 
-app.listen(8080, function(){
-	console.log('listening on 8080')
+	db = client.db('todoapp');
+
+	db.collection('post').insertOne( {이름 : 'John', 나이 : 20 }, function(err, result){
+		console.log('저장완료');
+	});
+
+  app.listen('8080', function(){
+		console.log('listening on 8080');
+	});
+  client.close();
 });
+
 
 app.get('/pet', function(req, res){
 	res.send('펫 용품 쇼핑할 수 있는 페이지입니다.');
@@ -29,3 +43,4 @@ app.post('/add', function(req, res){
 	console.log(req.body);
 	console.log(req.body.title);
 });
+
